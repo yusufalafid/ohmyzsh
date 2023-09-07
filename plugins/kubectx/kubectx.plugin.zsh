20931e5ea6c5c6bf0607dbe43920c92e991f5346
System Engineer 02 <syseng02@uucp.in>
2023-09-07 17:11:47 +0700

    add kubens namespaces name

--- plugins/kubectx/kubectx.plugin.zsh
+++ plugins/kubectx/kubectx.plugin.zsh
@@ -3,7 +3,11 @@ typeset -g -A kubectx_mapping
 function kubectx_prompt_info() {
   (( $+commands[kubectl] )) || return
 
-  local current_ctx=$(kubectl config current-context 2> /dev/null)
+  # local current_ctx=$(kubectl config current-context 2> /dev/null)
+  local current_ctx=$(kubens -c 2> /dev/null)
+  current_ctx+=" | $(kubectl config current-context | awk -F_ '{print $4}' 2> /dev/null)"
+
+  # local current_ctx=$(kubens -c 2> /dev/null)
 
   [[ -n "$current_ctx" ]] || return
 
